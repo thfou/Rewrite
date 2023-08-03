e7b50de3569b15b33cbc06e8845d9483355fad78
头号否 <45505928+thfou@users.noreply.github.com>
2023-08-03 11:17:10 +0800

    Update caiyuntianqiPro.js

--- caiyuntianqiPro.js
+++ caiyuntianqiPro.js
@@ -7,7 +7,7 @@ Token数据来源：@WeiRenQAQ
 **************************************
 
 [rewrite_local]
-^https?:\/\/(biz|wrapper)\.cyapi\.cn\/(.+\/(user.+|visitors|activity)|p\/v\d\/vip_info) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyun.js
+^https?:\/\/(biz|wrapper)\.cyapi\.cn\/(.+\/(user.+|visitors|activity)|p\/v\d\/vip_info) url script-response-body https://raw.githubusercontent.com/thfou/Rewrite/main/caiyuntianqiPro.js
 ^http:\/\/adx\.sogaha\.cn\/sdk\/ad\/get url reject-200
 
 [mitm]
